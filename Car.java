447de1027a475d1e55b9f41cba49e62722287058
Bethlan <118251473+Bethlan@users.noreply.github.com>
2022-12-01 15:41:20 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car_project;
+
+/**
+ *
+ * @author Bethlan Betty
+ */
+class Car {
+    String Car_brand;
+    String Car_model;
+    String Engine_no;
+    String Chase_no;
+    void get_details()
+    {
+            System.out.println("Car_brand"+Car_brand);
+            System.out.println("Car_model"+Car_model);
+            System.out.println("Engine_no"+Engine_no);
+            System.out.println("Chase_no"+Chase_no);
+}
+}
---
+++ Car_project.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package car_project;
+
+/**
+ *
+ * @author Bethlan Betty
+ */
+public class Car_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Car Audi=new Car();
+        Audi.Car_brand="Audi";
+        Audi.Car_model="A5";
+        Audi.Engine_no="3455654";
+        Audi.Chase_no="546654";
+        Audi.get_details();
+        
+        Car Renault=new Car();
+        Renault.Car_brand="Renault";
+        Renault.Car_model="Duster";
+        Renault.Engine_no="87654";
+        Renault.Chase_no="65765";
+        Renault.get_details();
+        
+        Car Bmw = new Car ();
+        Bmw.Car_brand= "Bmw";
+        Bmw.Car_model="a9";
+        Bmw.Engine_no= "0863";
+        Bmw.Chase_no= "86190";
+        Bmw.get_details();
+        
+        Car Maclaren=new Car();
+        Maclaren.Car_brand="Maclaren";
+        Maclaren.Car_model="720s";
+        Maclaren.Chase_no="4774";
+        Maclaren.Engine_no="7447";
+        Maclaren.get_details();
+        
+        
+     
+        
+    }
+}
+
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
